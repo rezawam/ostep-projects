67e961c52bbf0fc800bca051b7ee3811081c94ed
rezawam <rotten.swamp@mail.ru>
2025-10-03 11:20:08 +0300

    Add wcat

---
+++ initial-utilities/wcat/wcat.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv) {
+    for (size_t n = 1; n < argc; n++) 
+    {
+        FILE* fp = fopen(argv[n], "r");
+
+        if (fp == NULL) 
+        {
+            printf("wcat: cannot open file\n");
+            exit(1);
+        }        
+        char buff[10000];
+        while (fgets(buff, sizeof(buff), fp))
+        {
+            printf("%s", buff);
+        }
+        fclose(fp);
+    }
+
+    return 0;
+}
